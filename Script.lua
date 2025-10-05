4c580aacdbdfe033df4db4191f855cf0287b2be4
scripts <nielanthonyr@gmail.com>
2025-10-05 13:17:37 +0800

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/IuWKkHNQ/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/AjgYEhjV/raw", true))()
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/regata69f/ffs-public/refs/heads/main/loader.lua'))()
